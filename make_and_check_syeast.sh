48199048ac4423007190f6d976a74ac962305763
Patrick Wang <wang.patrick57@gmail.com>
2022-12-10 20:54:52 -0800

    deleted test script

--- make_and_check_syeast.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-PAUSE=0 NO8=1 NO7=1 make base
-./blant -k6 -lDEG1 -mi networks/syeast.el 2>/dev/null >syeast-test.txt
-diff -s syeast-test.txt indexes/syeast-k6-l1-dup.ind
-rm syeast-test.txt
